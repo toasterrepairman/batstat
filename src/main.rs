886ef8d823fe2daa3f944f8608b362c7bc6a6100
toast <smol@toast.cyou>
2021-12-15 18:56:35 -0500

    MVP for battery iterator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use gtk::prelude::*;
+use battery::*;
 
 fn main() {
     build_ui();
@@ -18,6 +19,22 @@ fn build_ui() {
     window.connect_destroy( |_| { 
         gtk::main_quit();
     });
+
+    let manager = battery::Manager::new().expect("Batteries not found");
+
+    // Battery display
+    for (idx, maybe_battery) in manager
+        .batteries()
+        .expect("Could not list batteries")
+        .enumerate() {
+        let battery = maybe_battery.expect("Could not initialize battery info");
+        let battery_label = gtk::Label::new(Some(&battery.state().to_string()));
+
+        
+        window.add(&battery_label);
+    }
+
+    
     // Present window to the user
     window.present();
     // Begin the GTK process
